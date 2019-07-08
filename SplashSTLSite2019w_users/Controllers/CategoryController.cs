f56297a06ccb0c6403f46c9922492221515c83cc
JCLYoung <jclyoung@gmail.com>
2019-07-08 00:58:44 -0500

    Complete

---
+++ SplashSTLSite2019w_users/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SplashSTLSite2019w_users.Data;
+using SplashSTLSite2019w_users.Models;
+using SplashSTLSite2019w_users.ViewModels;
+using SplashSTLSite2019w_users.ViewModels.Category;
+
+namespace SplashSTLSite2019w_users.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryCreateViewModel categoryCreateViewModel)
+        {
+            int id = CategoryCreateViewModel.CreateCategory(context, categoryCreateViewModel);
+            return RedirectToAction("Index", "Location");
+
+        }
+    }
+}
--- SplashSTLSite2019w_users/ViewModels/Category/CategoryCreateViewModel.cs
+++ SplashSTLSite2019w_users/ViewModels/Category/CategoryCreateViewModel.cs
@@ -1,31 +1,33 @@
 ﻿using SplashSTLSite2019w_users.Data;
+using SplashSTLSite2019w_users.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SplashSTLSite2019w_users.ViewModels
+namespace SplashSTLSite2019w_users.ViewModels.Category
 {
-        public class CategoryCreateViewModel
+    public class CategoryCreateViewModel
+    {
+        private ApplicationDbContext context;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public static int CreateCategory(ApplicationDbContext context, CategoryCreateViewModel categoryCreateViewModel)
         {
-            private ApplicationDbContext context;
-
-            public int Id { get; set; }
-
-            public string Name { get; set; }
-
-            public static int CreateCategory(ApplicationDbContext context, CategoryCreateViewModel categoryCreateViewModel)
+            Models.Category category = new Models.Category();
             {
-                Models.Category category = new Models.Category();
-                {
-                    category.Name = categoryCreateViewModel.Name;
-                }
-                context.Add(category);
-                context.SaveChanges();
-
-                return category.Id;
+                category.Name = categoryCreateViewModel.Name;
             }
+            context.Add(category);
+            context.SaveChanges();
+
+            return category.Id;
         }
-    
+    }
+
 }
 
